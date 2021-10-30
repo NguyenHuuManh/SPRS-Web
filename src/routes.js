40305e15e0f96cf6f4f07d43a6fba8d1b01b41d3
tuyenvmhe130902 <tuyen0143@gmail.com>
2021-10-30 23:57:21 +0700

    First commit

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,14 @@ const Users = React.lazy(() => import('./views/users/Users'));
 const User = React.lazy(() => import('./views/users/User'));
 const UserManagerment = React.lazy(() => import('./views/pages/userManagerment'));
 const OrgManagerment = React.lazy(() => import('./views/pages/orgManagerment'));
-
+const NewPage = React.lazy(() => import('./views/pages/newpage'));
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/users', exact: true, name: 'Users', component: Users },
   { path: '/quan-ly-user', exact: true, name: 'Quản Lý Người Dùng', component: UserManagerment },
   { path: '/quan-ly-org', exact: true, name: 'Quản Lý Tổ Chức', component: OrgManagerment },
-  { path: '/users/:id', exact: true, name: 'User Details', component: User }
+  { path: '/users/:id', exact: true, name: 'User Details', component: User },
+  { path: '/newpage', exact: true, name: 'New Page', component: NewPage }
 ];
 
 export default routes;
---
+++ src/views/pages/newpage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+NewPage.propTypes = {
+    
+};
+
+function NewPage(props) {
+    return (
+        <div>
+            Hello
+        </div>
+    );
+}
+
+export default NewPage;
